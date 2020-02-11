70c8cfed59d07a0eca16428056e95b9668726eed
Joel Burdorf <cozlikb@gmail.com>
2020-02-11 16:21:07 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// requires
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+//uses
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+//globals
+const port = 5000;
+
+//DB setup
+
+//server up
+app.listen(port, ()=>{
+console.log('server is up on', port);
+})
+//routes
