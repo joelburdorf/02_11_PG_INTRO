0e176c5102694d976c9446bd0ef7d3cfc5be6797
Joel Burdorf <cozlikb@gmail.com>
2020-02-11 16:29:00 -0600

    set up database configs on server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
 // requires
 const express = require('express');
-
 const app = express();
 const bodyParser = require('body-parser');
+const pg = require('pg');
 
 //uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+
 //globals
 const port = 5000;
 
 //DB setup
+const pool = new pg.Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 3000
+}); //end pool
 
 //server up
 app.listen(port, ()=>{
 console.log('server is up on', port);
-})
+}) //end serer set up
+
 //routes
