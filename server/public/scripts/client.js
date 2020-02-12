24818cddc978fbb0790641b6a09b3b1ccae03bfd
Joel Burdorf <cozlikb@gmail.com>
2020-02-12 10:26:00 -0600

    added getSongs to client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,24 @@
 console.log('in JS');
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in onReady')
+    getSongs();
+
+} // end onReady
+
+
+function getSongs(){
+    //make ajax request to GET songs from server
+    $.ajax({
+        type: 'GET',
+        url: '/songs'
+    }).then(function(response){
+
+        console.log('back from GET with ', response);
+    }).catch (function (err){
+        console.log( err);
+        alert ('no worky');
+    })
+}
